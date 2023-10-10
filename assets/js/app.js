3f5e42d179e6001692d806647024734e14e8364f
thtan0407 <thtan0407@example.com>
2023-10-10 19:48:45 +0700

    Update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,58 +1,92 @@
 $(function () {
-	if ($('#slider-hero').length) {
-		new Swiper('#slider-hero .swiper', {
-			speed: 500,
-			slidesPerView: 1,
-			preloadImages: false,
-			effect: 'fade',
-			loop: true,
-			autoplay: {
-				delay: 8000,
-				disableOnInteraction: false,
-			}, navigation: {
-				nextEl: "#slider-hero .banner-button_next",
-				prevEl: "#slider-hero .banner-button_prev",
-			}, pagination: {
-				el: "#slider-hero .banner-pagination",
-				clickable: true, renderBullet: function (index, className) {
-					return `<span class="${className}">0${index + 1}</span>`;
-				},
-			}
-		});
-	}
-	if ($('#slider-service').length) {
-		new Swiper('#slider-service .swiper', {
-			speed: 500,
-			slidesPerView: 5,
-			preloadImages: false,
-			spaceBetween: 24,
-			loop: true,
-			autoplay: {
-				delay: 6000,
-				disableOnInteraction: false,
-			},
-			pagination: {
-				el: "#slider-service .slider-pagination",
-				clickable: true
-			}
-		});
-	}
+    if ($('#slider-hero').length) {
+        new Swiper('#slider-hero .swiper', {
+            speed: 500,
+            slidesPerView: 1,
+            preloadImages: false,
+            effect: 'fade',
+            loop: true,
+            autoplay: {
+                delay: 8000,
+                disableOnInteraction: false,
+            }, navigation: {
+                nextEl: "#slider-hero .banner-button_next",
+                prevEl: "#slider-hero .banner-button_prev",
+            }, pagination: {
+                el: "#slider-hero .banner-pagination",
+                clickable: true, renderBullet: function (index, className) {
+                    return `<span class="${className}">0${index + 1}</span>`;
+                },
+            }
+        });
+    }
+    if ($('#slider-service').length) {
+        new Swiper('#slider-service .swiper', {
+            speed: 500,
+            slidesPerView: 5,
+            preloadImages: false,
+            spaceBetween: 24,
+            loop: true,
+            autoplay: {
+                delay: 6000,
+                disableOnInteraction: false,
+            },
+            pagination: {
+                el: "#slider-service .slider-pagination",
+                clickable: true
+            },
+            breakpoints: {
+                320: {
+                    slidesPerView: 1,
+                },
+                375: {
+                    slidesPerView: 1.25,
+                },
+                768: {
+                    slidesPerView: 2.5,
+                },
+                991: {
+                    slidesPerView: 3,
+                },
+                1200: {
+                    slidesPerView: 5,
+                }
+            },
+        });
+    }
 
-	if ($('#slider-partner').length) {
-		new Swiper('#slider-partner .swiper', {
-			speed: 500,
-			slidesPerView: 6,
-			preloadImages: false,
-			spaceBetween: 24,
-			loop: true,
-			autoplay: {
-				delay: 6000,
-				disableOnInteraction: false,
-			},
-			pagination: {
-				el: "#slider-partner .slider-pagination",
-				clickable: true
-			}
-		});
-	}
+    if ($('#slider-partner').length) {
+        new Swiper('#slider-partner .swiper', {
+            speed: 500,
+            slidesPerView: 6,
+            preloadImages: false,
+            spaceBetween: 24,
+            loop: true,
+            autoplay: {
+                delay: 6000,
+                disableOnInteraction: false,
+            },
+            pagination: {
+                el: "#slider-partner .slider-pagination",
+                clickable: true
+            },
+            breakpoints: {
+                320: {
+                    slidesPerView: 1,
+                },
+                375: {
+                    slidesPerView: 1.25,
+                },
+                768: {
+                    slidesPerView: 2.5,
+                },
+                991: {
+                    slidesPerView: 3,
+                },
+                1200: {
+                    slidesPerView: 5,
+                }
+            },
+        });
+    }
 });
